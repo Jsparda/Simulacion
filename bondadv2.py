cdffe6d7c6eea1d98b75a77469ba6af4942eb4d6
Jsparda <tom_john5_slash@hotmail.com>
2020-10-09 19:01:11 -0500

    Continuación de práctica 6 con la segunda versión

--- bondadv2.py
+++ bondadv2.py
@@ -19,8 +19,9 @@ def probPoisson(lambd,evento):
 H0 = 0.05
 lambd = 0.0
 proNueva = 0.0
+t = 0.0
 
-
+ti = []
 muestras = []
 frecuencia = {}
 liPro= []
@@ -52,14 +53,51 @@ a = [liPro[j] for j in range(len(liPro)) if j == liPro.index(liPro[j])] ##Busca 
 print("Arroje repetidos")
 
 print("#repetidos:",len(a))
+#frecuencia.pop(9)  ##Elimina al 9 que no tiene está en la lista
 print("#Frecuencias:",len(frecuencia))
 
-del frecuencia["9"]
-print("#Frecuencias:",len(frecuencia))
 
 j = 0
 for j in range(len(a)):
     frec = len(muestras)*a[j]
     frecuenciaEsp.append(frec)
-#print(frecuencia)
-#print(frecuenciaEsp)
+    
+for j in range(len(frecuenciaEsp)):
+    #for k in range(len(frecuenciaEsp)):
+    tiA = ((frecuencia[j]-frecuenciaEsp[j])**2)/frecuenciaEsp[j]
+    ti.append(tiA)
+    t = t + ti[j]
+print("t=",t,"len ti",len(ti))
+
+
+#========================Poisson==================================
+#Genero los valores aleatorios con Poisson
+
+poiss = np.random.poisson(lambd,5000)
+
+probPoiss = []
+
+j = 0
+
+for j in range(len(poiss)):
+    pro = probPoisson(lambd,poiss[j])
+    probPoiss.append(pro)
+print(probPoiss)
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
